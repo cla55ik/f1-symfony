12c57d922ebad383dd031d5acd51651ea5a73fb1
i.chelnokov <iva5420@yandex.ru>
2022-01-14 21:23:07 +0300

    create PilotApiController

--- src/Controller/Api/ApiFrontendInterface.php
+++ src/Controller/Api/ApiFrontendInterface.php
@@ -2,10 +2,12 @@
 
 namespace App\Controller\Api;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\Serializer;
 
 interface ApiFrontendInterface
 {
-    public function getAll(): Response;
-    public function getBy(): Response;
+    public function getAll(EntityManagerInterface $entityManager): Response;
+    public function getBy(EntityManagerInterface $entityManager): Response;
 }
--- src/Controller/Api/PilotApiController.php
+++ src/Controller/Api/PilotApiController.php
@@ -2,18 +2,49 @@
 
 namespace App\Controller\Api;
 
+use App\Entity\Pilot;
+use App\Repository\PilotRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Serializer;
 
-class PilotApiController implements ApiFrontendInterface
+#[Route('/api/pilot', name: 'api_pilot')]
+class PilotApiController extends AbstractController implements ApiFrontendInterface
 {
-
-    public function getAll(): Response
+    #[Route('', name: '')]
+    public function getAll(EntityManagerInterface $entityManager): JsonResponse
     {
-        // TODO: Implement getAll() method.
+        $pilots = $entityManager->getRepository(Pilot::class)->findAll();
+        foreach ($pilots as $pilot){
+            $arrayCollection[] = array(
+                'id' => $pilot->getId(),
+                'name' => $pilot->getName(),
+                'surname' => $pilot->getSurname(),
+                'comand' => [
+                    'id' => $pilot->getComand()->getId(),
+                    'name' => $pilot->getComand()->getName(),
+                    'img' => $pilot->getComand()->getImg()
+                ],
+                'country' => [
+                    'id' => $pilot->getCountry()->getId(),
+                    'name' => $pilot->getCountry()->getName(),
+                    'img' => $pilot->getCountry()->getImg()
+                ],
+                'img' => $pilot->getImg(),
+                'number' => $pilot->getNumber()
+            );
+        }
+
+        $data['items'] = $arrayCollection;
+        return new JsonResponse($data);
     }
 
-    public function getBy(): Response
+    #[Route('/{id}', name: '_id', requirements: ['id'=>'\d+'])]
+    public function getBy(EntityManagerInterface $entityManager): Response
     {
-        // TODO: Implement getBy() method.
+        return $this->json("PilotGetBy");
     }
 }
